debc59d4f0a5a1aef23246cbc35449e1d0588609
Zhipeng Wang <wangzhp85@gmail.com>
2018-08-10 00:52:01 +0900

    ddpg in progress, should be fun

--- ml_model.py
+++ ml_model.py
@@ -1,9 +1,10 @@
 #CopyRight no@none.not
-from keras.layers import Input
+from keras.layers import Input, Concatenate
 from keras.layers.core import Dense, Dropout
 from keras.optimizers import SGD
 from keras.models import Model
 import numpy as np
+from keras import backend as K
 
 
 class TwoHeadModel():
@@ -42,3 +43,54 @@ class TwoHeadModel():
         win_loses = [self.get_win_lose(i) for i in range(1, self.egg_total + 1)]
         return actions, win_loses
         
+
+def _neg_loss(y_true, y_pred):
+    return -K.mean(y_pred, axis = -1)
+    
+class DDPGModel():
+    def __init__(self, egg_total, max_egg_per_round):
+        self.inp = Input(shape = (egg_total, ))
+        self.hidden1 = Dense(egg_total, activation = 'relu')(inp)
+        drpout = Dropout(0.6)(self.hidden1)
+        self.actor = Dense(1, activation = 'tanh')(drpout)
+        self.hidden2 = Dense(egg_total / 2 + 2, activation = 'relu')(self.inp)
+        self.critic = Dense(1, activation = 'tanh')(Concatenate()([self.actor, self.hidden2]))
+        self.sgd = SGD(lr = 0.1)
+
+        self.egg_total = egg_total
+        self.max_egg_per_round = max_egg_per_round
+
+    def to_actor_net(self):
+        self.model = Model(inputs = self.inp, outputs = self.actor)
+
+    def to_critic_net(self):
+        self.model = Model(inputs = self.inp, outputs = self.actor)
+        
+    def to_critic_training_net(self):
+        self.hidden1.trainable = False
+        self.actor.trainable = False
+        self.hidden2.trainable = True
+        self.critic.trainable = True
+        self.model = Model(inputs = self.inp, outputs = self.critic)
+        self.model.compile(loss = 'mse', optimizer = self.sgd)
+
+    def to_actor_training_net(self):
+        self.hidden1.trainable = True
+        self.actor.trainable = True
+        self.hidden2.trainable = False
+        self.critic.trainable = False
+        self.model = Model(inputs = self.inp, outputs = self.critic)
+        self.model.compile(loss = _neg_loss, optimizer = self.sgd)
+
+    def get_action(self, egg_leftover):
+        assert egg_leftover <= self.egg_total
+        assert egg_leftover >= 1
+        arr = np.zeros((1, self.egg_total))
+        arr[0][egg_leftover - 1] = 1.0
+        self.to_actor_net()
+        pred = self.model.predict(arr)[0]
+        a = 0.5 * (self.max_egg_per_round - 1)
+        b = 0.5 * (self.max_egg_per_round + 1)
+        return int(a * pred + b)
+
+    
