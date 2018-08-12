0fbcbda55bae6be32bc4ead73eb686f801b12ea3
Zhipeng Wang <wangzhp85@gmail.com>
2018-08-12 15:19:12 +0900

    training as expected, trainable sucks

--- ml_model.py
+++ ml_model.py
@@ -49,14 +49,14 @@ def _neg_loss(y_true, y_pred):
     
 class DDPGModel():
     def __init__(self, egg_total, max_egg_per_round):
-        self.inp = Input(shape = (egg_total, ))
-        self.hidden1 = Dense(egg_total, activation = 'relu')(self.inp)
+        self.inp = Input(shape = (egg_total, ), name = 'inp')
+        self.hidden1 = Dense(egg_total, activation = 'relu', name = 'hid1')(self.inp)
         drpout1 = Dropout(0.6)(self.hidden1)
-        self.actor = Dense(1, activation = 'tanh')(drpout1)
-        self.hidden2 = Dense(egg_total / 2 + 2, activation = 'relu')(self.inp)
-        self.hidden3 = Dense(egg_total / 2 + 3, activation = 'relu')(Concatenate()([self.actor, self.hidden2]))
+        self.actor = Dense(1, activation = 'tanh', name = 'actor')(drpout1)
+        self.hidden2 = Dense(egg_total / 2 + 2, activation = 'relu', name = 'hid2')(self.inp)
+        self.hidden3 = Dense(egg_total / 2 + 3, activation = 'relu', name = 'hid3')(Concatenate()([self.actor, self.hidden2]))
         drpout2 = Dropout(0.6)(self.hidden3)
-        self.critic = Dense(1, activation = 'tanh')(drpout2)
+        self.critic = Dense(1, activation = 'tanh', name = 'critic')(drpout2)
         self.sgd = SGD(lr = 0.1)
 
         self.egg_total = egg_total
@@ -72,24 +72,24 @@ class DDPGModel():
         self.model = Model(inputs = self.inp, outputs = self.actor)
 
     def train_critic_net(self, data_label):
-        self.hidden1.trainable = False
-        self.actor.trainable = False
-        self.hidden2.trainable = True
-        self.hidden3.trainable = True
-        self.critic.trainable = True
         self.model = Model(inputs = self.inp, outputs = self.critic)
+        self.model.get_layer('hid1').trainable = False
+        self.model.get_layer('actor').trainable = False
+        self.model.get_layer('hid2').trainable = True
+        self.model.get_layer('hid3').trainable = True
+        self.model.get_layer('critic').trainable = True
         self.model.compile(loss = 'mse', optimizer = self.sgd)
-        self.model.fit(data_label[0], data_label[1], batch_size = 5, epochs = 30)
+        self.model.fit(data_label[0], data_label[1], batch_size = 5, epochs = 30, verbose = 0)
             
     def train_actor_net(self, data_label):
-        self.hidden1.trainable = True
-        self.actor.trainable = True
-        self.hidden2.trainable = False
-        self.hidden3.trainable = False
-        self.critic.trainable = False
         self.model = Model(inputs = self.inp, outputs = self.critic)
+        self.model.get_layer('hid1').trainable = True
+        self.model.get_layer('actor').trainable = True
+        self.model.get_layer('hid2').trainable = False
+        self.model.get_layer('hid3').trainable = False
+        self.model.get_layer('critic').trainable = False
         self.model.compile(loss = _neg_loss, optimizer = self.sgd)
-        self.model.fit(data_label[0], data_label[1], batch_size = 5, epochs = 1)
+        self.model.fit(data_label[0], data_label[1], batch_size = 5, epochs = 3, verbose = 0)
         
     def get_action(self, egg_leftover, explore = False):
         assert egg_leftover <= self.egg_total
@@ -115,3 +115,11 @@ class DDPGModel():
         actions = [self.get_action(i) for i in range(1, self.egg_total + 1)]
         win_loses = [self.get_critic(i)[0] for i in range(1, self.egg_total + 1)]
         return actions, win_loses
+
+    def get_model_detail(self):
+        data = np.eye(self.egg_total)
+        actions = Model(inputs = self.inp, outputs = self.actor).predict(data)
+        hd2_oup = Model(inputs = self.inp, outputs = self.hidden2).predict(data)
+        hd3_oup = Model(inputs = self.inp, outputs = self.hidden3).predict(data)
+        return actions, hd2_oup, hd3_oup
+        
