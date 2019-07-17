9e5e743ab59520399283eae84f2e7e3aff9beb49
Zhipeng Wang <wangzhp85@gmail.com>
2019-07-17 23:38:43 +0900

    code lines 30 :)

--- KuhnPoker.py
+++ KuhnPoker.py
@@ -1,56 +1,36 @@
 import numpy as np
 
 num_actions = 2
+nodeMap = {}
 
 class cfrNode:
-
     def __init__(self):
         self.regretSum = np.zeros([num_actions])
         self.strategySum = np.zeros([num_actions])
-    
+        
     def getStrategy(self):
         ind = self.regretSum > 0
-        normalizedSum = np.sum(self.regretSum[ind])
-        if not normalizedSum > 0:
-            return np.ones([num_actions])/num_actions
-        strategy = np.zeros([num_actions])
-        strategy[ind] = self.regretSum[ind]/normalizedSum
-        return strategy
-
+        return (lambda nSum: np.ones([num_actions]) / num_actions if not nSum > 0 else np.maximum(self.regretSum, 0.0)/nSum)(np.sum(self.regretSum[ind]))
+    
     def getAverageStrategy(self):
         return self.strategySum/np.sum(self.strategySum)
-
+    
     def update(self, regret, strategy):
         self.regretSum += regret
         self.strategySum += strategy
     
-nodeMap = {}
-
 def cfr(cards, history, p0, p1):
     player = len(history) % 2
-    isPlayerBetter = (cards[player] > cards[1-player])
-    
-    if history in ['pbb', 'bb']:
-        return 2 if isPlayerBetter else -2
-    elif history in ['pp']:
-        return 1 if isPlayerBetter else -1
-    elif history in ['pbp', 'bp']:
-        return 1
-
+    isPlayerBetter = 1 if cards[player] > cards[1-player] else -1
+    end_states = {'pbb': 2 * isPlayerBetter, 'bb': 2 * isPlayerBetter, 'pp': isPlayerBetter, 'pbp': 1, 'bp': 1}
+    if history in end_states: return end_states[history]
     infoSet = str(cards[player]) + history
-    if infoSet not in nodeMap:
-        nodeMap[infoSet] = cfrNode()
-    
+    if infoSet not in nodeMap: nodeMap[infoSet] = cfrNode()
     strategy = nodeMap[infoSet].getStrategy()
-    util = np.array([-cfr(cards, history + ['p', 'b'][a], p0 * strategy[a], p1) if player == 0 else -cfr(cards, history + ['p', 'b'][a], p0, p1 * strategy[a]) for a in range(num_actions)])
-    
-    nodeUtil = np.sum(util * strategy)
+    util = np.array([-cfr(cards, history + ['p', 'b'][a], p0 * [strategy[a], 1.0][player], p1 * [1.0, strategy[a]][player]) for a in range(num_actions)])
+    nodeUtil = np.dot(util, strategy)
     nodeMap[infoSet].update((util - nodeUtil) * (p1 if player == 0 else p0), strategy * (p0 if player == 0 else p1))
-
     return nodeUtil
 
-for _ in range(50000):
-    cfr(np.random.permutation([1, 2, 3]), '', 1.0, 1.0)
-
-for s in nodeMap:
-    print(s, nodeMap[s].getAverageStrategy())
+for _ in range(50000): cfr(np.random.permutation([1, 2, 3]), '', 1.0, 1.0)
+print('\n'.join([s + ':' + str(nodeMap[s].getAverageStrategy()) for s in nodeMap]))
